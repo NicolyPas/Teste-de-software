878977d41505d05a283b45cacc270d978aedd088
NicolyPas <108129610+NicolyPas@users.noreply.github.com>
2022-06-24 09:11:52 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,29 @@
+import java.lang.management.ThreadInfo;
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    Integer idade;
+    String sexo;
+
+    System.out.println("Digite sua idade em anos: " );
+        idade = input.nextInt();
+
+    System.out.println("Digite seu sexo: " );
+      sexo = input.nextStr();  
+      
+      if ((idade > 17) && (sexo == "M")) {
+        System.out.println("true");
+      }
+       if ((idade < 18) && (sexo == "f")) {
+       System.out.println( "false");
+       }
+    
+
+    
+
+
+        
+    }
+}
